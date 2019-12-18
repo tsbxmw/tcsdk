0c55dac58080fb7fb7cfab50d44f379fc20f7d2e
mengwei <mengwei@mengwei.local>
2019-12-18 17:14:38 +0800

    add test file; update api

---
+++ tests/test_upload.py
@@ -0,0 +1,29 @@
+from tcsdk import *
+from random import randint
+
+
+if __name__ == "__main__":
+    auth = Auth("c4986f87", "43c144b4-99ec-48")
+    api = Api(auth=auth, endpoint="http://localhost:9030")
+    task = api.create_task("test_3")
+    assert task.task_id != 0
+
+    label = task.create_label("test_3")
+    assert label.label_id != 0
+
+    for i in range(1):
+        data = dict(
+            fps=randint(1, 60),
+            cpu_total=randint(40, 100),
+            cpu_app = randint(1,40),
+            memory_real=randint(100, 4000),
+            memory_total = randint(100,4000),
+            memory_virtual=randint(100, 1000),
+            network_send = randint(1,40000),
+            network_receive=randint(1, 40000),
+            gpu_rendor = randint(40,100),
+            gpu_tiler = randint(1,40),
+            gpu_device = randint(1,40)
+        )
+
+        assert label.upload(**data) is True
